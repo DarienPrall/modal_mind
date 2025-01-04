ab07c53962a0cc0cdf18076ac9db4a37be49a152
Darien <galleon_caboose_0r@icloud.com>
2025-01-04 12:38:59 -0700

    Commit 1: Project Setup
    Contains two files
    modal_mind.py is for personal programming
    ITFPX4534 is for school assessment
    
    Both have been structured with comments to align with the project plan: https://github.com/users/DarienPrall/projects/3/views/1

---
+++ modal_mind.py
@@ -0,0 +1,80 @@
+#----------------------------------------------------------------------
+#
+# PROJECT INFORMATION
+# Title: ModalMind
+# Author: Darien Prall
+# Start Date: 01-04-2025 (mmddyyyy)
+# End Date: 
+# Project Description: For this project, I explore the usage of modal verbs in different texts from the Gutenberg Corpus. I will analyze the relative frequency through different Natural Language Processing (NLP) techniques.
+#
+# PROJECT STEPS
+# Task 0: Import and Clean Data
+#
+# Task 1: Modals in the Gutenberg Corpus
+# - Task 1.1: Install NLTK
+# - Task 1.2: Download the Gutenberg Corpus
+# - Task 1.3: Define Each Modal Group
+# - Task 1.4: Count Relative Frequencies of Each Modal Verb
+# - Task 1.5: Find the Texts with the Largest Span of Modal Frequencies
+# - Task 1.6: Compare Usage of Modals in the Two Texts
+#
+# Task 2: Inaugural Corpus - Analyzing Kennedy's 1961 Speech
+# - Task 2.1: Download the Inaugural Corpus
+# - Task 2.2: Load the 1961 Kennedy Speech
+# - Task 2.3: Identify the 10 Most Frequent Used Long Words
+# - Task 2.4: Use WordNet to Find Synonyms and Hyponyms
+# - Task 2.5: Reflect of the Results
+#----------------------------------------------------------------------
+
+#----------------------------------------------------------------------
+# Task 1: Modals in the Gutenberg Corpus
+
+# - Task 1.1: Install NLTK
+# What this code is doing:
+#
+
+# - Task 1.2: Download the Gutenberg Corpus
+# What this code is doing:
+#
+
+# - Task 1.3: Define Each Modal Group
+# What this code is doing:
+#
+
+# - Task 1.4: Count Relative Frequencies of Each Modal Verb
+# What this code is doing:
+#
+
+# - Task 1.5: Find the Texts with the Largest Span of Modal Frequencies
+# What this code is doing:
+#
+
+# - Task 1.6: Compare Usage of Modals in the Two Texts
+# What this code is doing:
+#
+
+#----------------------------------------------------------------------
+
+#----------------------------------------------------------------------
+# Task 2: Inaugural Corpus - Analyzing Kennedy's 1961 Speech
+
+# - Task 2.1: Download the Inaugural Corpus
+# What this code is doing:
+#
+
+# - Task 2.2: Load the 1961 Kennedy Speech
+# What this code is doing:
+#
+
+# - Task 2.3: Identify the 10 Most Frequent Used Long Words
+# What this code is doing:
+#
+
+# - Task 2.4: Use WordNet to Find Synonyms and Hyponyms
+# What this code is doing:
+#
+
+# - Task 2.5: Reflect of the Results
+# What this code is doing:
+#
+#----------------------------------------------------------------------
